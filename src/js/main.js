45c7f6119ba561694c270abd7cab72075c7b2ca3
Mariusz Flis <mariusz.flis@gmail.com>
2019-12-05 12:24:36 +0100

    feat:hamburger menu, main image

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
       // Registration was successful
@@ -12,11 +12,32 @@ if ('serviceWorker' in navigator) {
       console.log('ServiceWorker registration failed: ', err);
     });
   });
-}
+}*/
 
 // place your code below
+const navSlide = () => {
+  const burgerMenu = document.querySelector('.nav__burgerMenu');
+  const nav = document.querySelector('.nav__links');
+  const navLinks = document.querySelectorAll('.nav__list');
+  //Przełacznik navigacji
+  burgerMenu.addEventListener('click',() => {
+      nav.classList.toggle('nav__active');
+
+      navLinks.forEach((link, index)=> {
+          if (link.style.animation){
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +0.5}s`;
+          }
+      });
+
+      //Burger animacja
+     burgerMenu.classList.toggle('toggle');
+
+  });   
+}
+navSlide();
 
 
-console.log(`Hello world!`);
 
 
